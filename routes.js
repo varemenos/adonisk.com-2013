c321ec63b190f1894bc8e4dd343f1b1c495f1c44
Adonis K <aklp08@gmail.com>
2014-01-23 14:45:12 +0200

    added the prefixer's service route and views

--- routes.js
+++ routes.js
@@ -2,14 +2,19 @@ exports.index = function(req, res){
   res.render('index', {});
 };
 
-exports.unicode = function(req, res){
-  res.render('unicode', {});
+exports.formatter = function(req, res){
+  res.render('formatter', {});
+};
+
+exports.prefixer = function(req, res){
+  res.render('prefixer', {});
 };
 
 exports.base64 = function(req, res){
   res.render('base64', {});
 };
 
-exports.formatter = function(req, res){
-  res.render('formatter', {});
+
+exports.unicode = function(req, res){
+  res.render('unicode', {});
 };
