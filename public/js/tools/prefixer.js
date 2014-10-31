3c3d4ca6ce3433c08f931246f5e5bad7cdd3c366
Adonis K <aklp08@gmail.com>
2014-10-31 23:22:58 +0200

    basic functionality for prefixer

--- public/js/tools/prefixer.js
+++ public/js/tools/prefixer.js
@@ -2,12 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	var editor = ace.edit('edit');
 	var output = ace.edit('output');
 
-	editor.setValue('a{\n  display: flex;\n}');
-	output.setValue('a{\n  display: -webkit-box;\n  display: -webkit-flex;\n  display: -moz-box;\n  display: -ms-flexbox;\n  display: flex;\n}');
-
-	editor.clearSelection();
-	output.clearSelection();
-
 	editor.setOptions({
 		mode: 'ace/mode/css',
 		theme: 'ace/theme/tomorrow_night_eighties',
@@ -44,16 +38,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	var settingsBtn = document.querySelector('#action-settings');
 	var spacesBtn = document.querySelector('#action-spaces');
-	var settings = document.querySelector('#settings');
+	var prefixBtn = document.querySelector('#action-prefix');
+	// var settings = document.querySelector('#settings');
 	var tabsSel = document.querySelector('select[name=tabs]');
 
-	settingsBtn.addEventListener('click', function () {
-		if (settings.classList.contains('active')) {
-			settings.classList.remove('active');
-		} else {
-			settings.classList.add('active');
-		}
-	});
+	// settingsBtn.addEventListener('click', function () {
+	// 	if (settings.classList.contains('active')) {
+	// 		settings.classList.remove('active');
+	// 	} else {
+	// 		settings.classList.add('active');
+	// 	}
+	// });
 
 	spacesBtn.addEventListener('click', function () {
 		editor.setOption('useSoftTabs', spacesBtn.checked);
@@ -63,5 +58,17 @@ document.addEventListener('DOMContentLoaded', function () {
 	tabsSel.addEventListener('change', function () {
 		editor.setOption('tabSize', tabsSel.value);
 		output.setOption('tabSize', tabsSel.value);
+	});
+
+	prefixBtn.addEventListener('click', function () {
+		var css = editor.getValue();
+		var prefixed;
+		try {
+			prefixed = autoprefixer().process(css).css;
+		} catch (e) {
+			prefixed = e.reason + ' at line ' + e.line;
+		}
+
+		output.setValue(prefixed, 1);
 	});
 });
