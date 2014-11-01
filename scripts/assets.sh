2551bc4d92c792aea086ce7d4809aae42cc35e61
Adonis K <aklp08@gmail.com>
2014-11-01 12:24:39 +0200

    added autoprefixer script

--- scripts/assets.sh
+++ scripts/assets.sh
@@ -1,3 +1,5 @@
 cp public/libs/font-awesome/css/font-awesome.css public/css/_font-awesome.scss
 cp public/libs/font-awesome/fonts/* public/fonts/
 cp public/libs/verepo/_verepo.scss public/css/
+curl https://raw.githubusercontent.com/ai/autoprefixer-rails/master/vendor/autoprefixer.js --create-dirs -o public/libs/autoprefixer/autoprefixer.js
+cp public/libs/autoprefixer/autoprefixer.js public/js/autoprefixer/
